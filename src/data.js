c013760875ddc6bdb6c5963ecd196c442755793c
honey khra <honeykhra@gmail.com>
2022-01-30 20:33:02 +0530

    Initial one

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1600545051343-d15fe93f6e67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "SUMMER SALE",
+    description: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1513094735237-8f2714d57c13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHNob3BwaW5nJTIwYmFnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    title: "WINTER SALE",
+    description: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1560884392-a636e0722bc4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTF8fHNob3BwaW5nJTIwYmFnfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=400&q=60",
+    title: "POPULAR SALE",
+    description: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+];
